21a5ced348052cd0ad3f3688d5daeff34bea3188
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-27 09:34:43 +0200

    n

---
+++ 1_f-c.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.leyers import Dense
+from tensorflow.random import set_seed
+
+
