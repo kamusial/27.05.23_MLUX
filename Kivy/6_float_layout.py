c5d4667a6add2d1599a43f46b8b009f14ba51272
student-wroclaw <student-wroclaw@EDU-WSB.local>
2023-05-28 14:07:30 +0200

    1

--- Kivy/6_float_layout.py
+++ Kivy/6_float_layout.py
@@ -7,10 +7,11 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.properties import ObjectProperty
+from kivy.uix.floatlayout import FloatLayout
 
 class My6App(App):
     def build(self):
-        return Label(text='Hello')
+        return FloatLayout()
 
 if __name__ == '__main__':
     My6App().run()
